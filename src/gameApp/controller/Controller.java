8ed81a289213e501aa0e8326909231f3622fb8de
Igor Frolov <shayzeq@gmail.com>
2018-05-15 20:19:16 +0300

    added 1 but in controller

--- src/gameApp/controller/Controller.java
+++ src/gameApp/controller/Controller.java
@@ -1,4 +1,18 @@
 package gameApp.controller;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML private Button but1;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
